824a4989af5ff42997ea99506379121186ee3e7f
febrezio <andreja.hernandez@gmail.com>
2022-12-19 02:43:40 -0800

    Added MIB table row index converter

--- src/RowIndexConverter.java
+++ src/RowIndexConverter.java
@@ -1,38 +1,59 @@
 import org.snmp4j.smi.OID;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class RowIndexConverter {
+
+    private static final Map<String, String> _6BitAsciiMap = Stream.of(new String[][] {
+            { "000000", "NULL" },
+            { "000001", "A" },
+            { "000010", "B" },
+            { "000011", "D" },
+            { "000100", "E" },
+            { "000101", "F" },
+            { "000110", "G" },
+            { "000111", "H" },
+            { "001000", "I" },
+            { "001001", "J" },
+            { "001010", "K" },
+            { "001011", "L" },
+            { "001100", "M" },
+            { "001101", "N" },
+            { "001110", "O" },
+            { "001111", "P" },
+            { "010000", "Q" },
+            { "010001", "R" },
+            { "010010", "S" },
+            { "010011", "T" },
+            { "101010", "U" },
+    }).collect(Collectors.toMap(data -> data[0], data -> data[1]));
     public String oidTo6BitAscii(OID oid) {
-        StringBuilder _6BitAsciiSb = new StringBuilder();
+        StringBuilder bitsStringBuilder = new StringBuilder();
         for (int decimalValue : oid.getValue()) {
-            _6BitAsciiSb.append(decimalTo6BitAscii(decimalValue));
+            bitsStringBuilder.append(Integer.toBinaryString(decimalValue));
         }
+        // Split into 6 bit segments
+        List<String> _6BitSegments = get6BitSegments(bitsStringBuilder.toString());
+        // For each 6 bit segment, convert to 6 Bit ASCII
+        StringBuilder _6BitAsciiSb = new StringBuilder();
+        for (String _6BitSegment : _6BitSegments) {
+            _6BitAsciiSb.append(_6BitAsciiMap.get(_6BitSegment));
+        }
+
         return _6BitAsciiSb.toString();
     }
-    private String decimalTo6BitAscii(int decimalValue) {
-        String _6BitHex = decimalTo6BitHex(decimalValue);
-        // TODO Find ascii code in 6bit ASCII table
-        return _6BitHex;
-    }
-    public String decimalTo6BitHex(int decimalValue) {
-        int _6BitDecimal = decimalTo6BitDecimal(decimalValue);
-        // Convert decimal to hex
-        return Integer.toHexString(_6BitDecimal);
-    }
-    private int decimalTo6BitDecimal(int decimalValue) {
-        String _6BitBinary = decimalTo6BitBinary(decimalValue);
-        // Convert bits to decimal
-        return Integer.parseUnsignedInt(_6BitBinary, 2);
-    }
-    private String decimalTo6BitBinary(int decimalValue) {
-        // 1) Flip the nibble order
-        int decimalSwappedNibibles = swapNibbles(decimalValue);
-        // 2) Convert decimal to binary
-        String binary = Integer.toBinaryString(decimalSwappedNibibles);
-        // 3) Get last 6 bits
-        return binary.substring(binary.length() - Math.min(binary.length(), 6));
-    }
-    private int swapNibbles(int x)
-    {
-        return ((x & 0x0F) << 4 | (x & 0xF0) >> 4);
+    private List<String> get6BitSegments(String binaryString) {
+        List<String> results = new ArrayList<>();
+        int length = binaryString.length();
+
+        for (int i = 0; i < length; i += 6) {
+            results.add(binaryString.substring(i, Math.min(length, i + 6)));
+        }
+
+        return results;
     }
 }
--- src/RowIndexConverterTest.java
+++ src/RowIndexConverterTest.java
@@ -12,13 +12,8 @@ public class RowIndexConverterTest {
         rowIndexConverter = new RowIndexConverter();
     }
     @Test
-    public void decimalTo6BitHexTest() {
-        String result = rowIndexConverter.decimalTo6BitHex(85);
-        assertEquals("15", result);
-    }
-    @Test
     public void oidTo6BitAsciiTest() {
-        String result = rowIndexConverter.oidTo6BitAscii(new OID("85.49"));
-        assertEquals("1513", result);
+        String result = rowIndexConverter.oidTo6BitAscii(new OID("85.49.90"));
+        assertEquals("USE", result);
     }
 }
